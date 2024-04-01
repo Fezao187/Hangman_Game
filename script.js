92a3778fb62dfad808d67e043be7732c8e9d47bb
Katlego Maredi <katlegomaredi9@gmail.com>
2024-04-01 21:57:14 +0200

    JavaScript: Selecting all page elements

---
+++ script.js
@@ -0,0 +1,15 @@
+"Use strict"
+
+//Selecting page elements as JS objects
+let head = document.querySelector(".head"),
+body = document.querySelector(".body"),
+handLeft = document.querySelector(".handleft"),
+handRight = document.querySelector(".handright"),
+footLeft = document.querySelector(".footleft"),
+footRight = document.querySelector(".footright"),
+//Selecting all the alphabets
+alphabetsArr = document.querySelectorAll(".alphanet");
+
+/**TODO: Code the hangman game, I have no idea how to do it
+ * at the moment but I will see. When I have time.
+ */
